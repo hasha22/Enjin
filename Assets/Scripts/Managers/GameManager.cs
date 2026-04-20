fdfe375297177d9f7ca70c47c8555c9c3d62c58a
Giuseppe Evangelista <evanggiuse@gmail.com>
2026-04-20 14:27:53 +0200

    The Layout Update Pt. 1
    Adds the basic layout and variable assignment for the Character info, situation explanation and policy voting 1 screens.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -9,6 +11,22 @@ public class GameManager : MonoBehaviour
     public List<Topic> allTopics = new List<Topic>();
     [SerializeField] private Topic currentTopic;
     [SerializeField] private Policy currentPolicy;
+
+    [Header("Main references")]
+    public List<GameObject> allScreens = new List<GameObject>();
+    public GameObject timer;
+    public TextMeshProUGUI timerText;
+
+
+    [Header("Situation Explanation Screen References")]
+    public TextMeshProUGUI scenarioNameText;
+    public TextMeshProUGUI scenarioDescriptionText;
+
+    [Header("Policy Voting 1 Screen References")]
+    public TextMeshProUGUI policyText1;
+    public GameObject keywordCardPrefab;
+    public GameObject posKeywordContainer;
+    public GameObject negKeywordContainer;
 
     void Awake()
     {
@@ -22,5 +40,4 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
 }
